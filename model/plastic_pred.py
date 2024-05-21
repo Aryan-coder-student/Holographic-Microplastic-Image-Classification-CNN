c81d2f35115768030068507c95d858b9df837a9d
Aryan Pahari <76030680+Aryan-coder-student@users.noreply.github.com>
2024-05-21 09:46:58 +0530

    error

--- model/plastic_pred.py
+++ model/plastic_pred.py
@@ -47,7 +47,7 @@ class PlasticPredict:
     def pred(self):
         device = "cuda" if torch.cuda.is_available() else "cpu"
         loaded_model = CNNModel().to(device)
-        loaded_model.load_state_dict(torch.load('model\micro_plastic.pth'))
+        loaded_model.load_state_dict(torch.load('./model/micro_plastic.pth'))
         input_tensor = self.img
         # print(input_tensor)
         input_batch = input_tensor.unsqueeze(0)
