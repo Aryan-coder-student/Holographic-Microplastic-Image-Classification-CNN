128e64fb605fd35afc7d002130f77ce8b4718773
Aryan Pahari <76030680+Aryan-coder-student@users.noreply.github.com>
2024-06-08 23:17:34 +0530

    Update About.py

--- About.py
+++ About.py
@@ -8,7 +8,7 @@ import google.generativeai as genai
 import time
 import os
 
-API_KEY = os.getenv("GEMNI_API_KEY")
+API_KEY = "AIzaSyDn6YVysXFhKrYSsgsriF592Ev9X8UmQps"
 
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
